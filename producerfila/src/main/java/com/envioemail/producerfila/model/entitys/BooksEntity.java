23c7a073fcbea1685a9ffcc9264ff46145918acb
RomulloSilva <romullo.dev@gmail.com>
2021-10-18 18:49:02 -0300

    Criação das classes para o BookProperties

--- producerfila/src/main/java/com/envioemail/producerfila/model/entitys/BooksEntity.java
+++ producerfila/src/main/java/com/envioemail/producerfila/model/entitys/BooksEntity.java
@@ -23,33 +23,33 @@ public class BooksEntity implements Serializable {
     @Column(name = "book_id", unique = true, nullable = false)
     private Integer id;
 
-    @Column(name = "book_title", unique = true, nullable = false)
+    @Column(name = "book_title", nullable = false)
     private String bookTitle;
 
-    @Column(name = " summary", unique = true, nullable = false)
+    @Column(name = "summary", nullable = false)
     private String summary;
 
-    @Column(name = "author_name", unique = true, nullable = false)
+    @Column(name = "author_name", nullable = false)
     private String authorName;
 
-    @Column(name = "author_id", unique = true, nullable = false)
+    @Column(name = "author_id", nullable = false)
     private Integer authorId;
 
-    @Column(name = "release_date", unique = true, nullable = false)
+    @Column(name = "release_date", nullable = false)
     private LocalDateTime releaseDate;
 
-    @Column(name = "volume", unique = true, nullable = false)
+    @Column(name = "volume", nullable = false)
     private Integer volume;
 
     @Column(name = "isbn", unique = true, nullable = false)
     private Integer isbn;
 
-    @Column(name = "number_of_pages", unique = true, nullable = false)
+    @Column(name = "number_of_pages", nullable = false)
     private Integer numberOfPages;
 
-    @Column(name = "country", unique = true, nullable = false)
+    @Column(name = "country", nullable = false)
     private String country;
 
-    @Column(name = "publishing_company", unique = true, nullable = false)
+    @Column(name = "publishing_company", nullable = false)
     private String publishingCompany;
 }
